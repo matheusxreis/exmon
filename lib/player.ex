fa6dee338277dfe67595cf9c1d1fc4cbc7aff050
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 21:15:58 -0300

    feat: build function in ExMon.Player module done to create the struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,6 +1,17 @@
+# unlike Map type, Struct has to be yours keys defined
 defmodule ExMon.Player do
 
   @enforce_keys [:life, :name, :move_random, :move_average, :move_heal]
   defstruct [:life, :name, :move_random, :move_average, :move_heal]
+
+  def build(name, move_random, move_average, move_heal) do
+    %ExMon.Player{
+      name: name,
+      move_random: move_random,
+      move_average: move_average,
+      move_heal: move_heal,
+      life: 100
+    }
+  end
 
 end
