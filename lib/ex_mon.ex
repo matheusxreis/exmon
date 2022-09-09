90eeb475d57a23bf6dfdfe719c73fa1d9658c84d
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 22:12:38 -0300

    feat: created start_game function in main module

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,8 +1,13 @@
 defmodule ExMon do
   alias ExMon.Player
+  alias ExMon.Game
 
   def create_player(n, m1, m2, m3) do
     Player.build(n, m1, m2, m3)
   end
 
+  def start_game(player) do
+    computer_player = create_player("Vin Diesel", :punch, :kick, :heal)
+    Game.start(computer_player, player)
+  end
 end
