ce30f20be7ee3bd8c25e3e26150ea15b82d8162c
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 17:18:18 -0300

    refactor: transforming all moves of Player struct in one Map

--- lib/player.ex
+++ lib/player.ex
@@ -1,7 +1,7 @@
 # unlike Map type, Struct has to be yours keys defined
 defmodule ExMon.Player do
 
-  @required_keys [:life, :move_average, :move_heal, :move_random, :name]
+  @required_keys [:life, :moves, :name]
   @max_life 100
 
   @enforce_keys @required_keys
@@ -10,9 +10,11 @@ defmodule ExMon.Player do
   def build(name, move_random, move_average, move_heal) do
     %ExMon.Player{
       life: @max_life,
-      move_average: move_average,
-      move_heal: move_heal,
-      move_random: move_random,
+      moves: %{
+        move_average: move_average,
+        move_heal: move_heal,
+        move_random: move_random,
+      },
       name: name,
     }
   end
