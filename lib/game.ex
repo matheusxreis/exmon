cdc66d8ad16fe44259a62b88049e93c62aa33f94
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 18:34:26 -0300

    feat: create function update in ExMon.Game module, to update the game state

--- lib/game.ex
+++ lib/game.ex
@@ -6,8 +6,14 @@ defmodule ExMon.Game do
     Agent.start_link(fn -> initial_value end,name: __MODULE__)
   end
 
+  def update(state) do
+  Agent.update(__MODULE__,fn _ -> state end)
+  end
+
+
   def info(), do: Agent.get(__MODULE__, fn it -> it end);
   def player(), do: Map.get(info(), :player)
   def fetch_player(p), do: Map.get(info(), p)
   def turn(), do: Map.get(info(), :turn)
+
 end
