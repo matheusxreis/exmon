c52f32e17afbf350c184c871ec3808eaf0b5ae37
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 18:24:45 -0300

    feat: create update_opponent_life function in Actions.Attack module

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -13,6 +13,7 @@ defmodule ExMon.Game.Actions.Attack do
       |> Game.fetch_player()
       |> Map.get(:life)
       |> calculate_total_life(damage)
+      |> update_opponent_life(target)
   end
 
   defp calculate_power(:move_random), do: Enum.random(@move_random_power)
@@ -20,5 +21,11 @@ defmodule ExMon.Game.Actions.Attack do
 
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
+
+  defp update_opponent_life(life, opponent) do
+    opponent
+    |> Game.fetch_player()
+    |> Map.put(:life, life)
+  end
 
 end
