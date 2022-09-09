13300f6f0697ec0fa03781d5a06eb60bc4ec755d
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 20:29:38 -0300

    feat: created function update_turn in ExMon.Game module

--- lib/game.ex
+++ lib/game.ex
@@ -21,9 +21,13 @@ defmodule ExMon.Game do
     %{player: %{life: player_life}, computer: %{life: computer_life}} = state
   ) when player_life == 0 or computer_life == 0, do: Map.put(state, :status, :game_over)
 
-  defp update_game_state(state) do
+  defp update_game_status(state) do
     state
-    |> Map.put(state, :status, :continue)
+    |> Map.put(:status, :continue)
     |> update_turn()
   end
+
+  defp update_turn(%{turn: :computer} = state), do: Map.put(state, :turn, :player)
+  defp update_turn(%{turn: :player} = state), do: Map.put(state, :turn,:computer)
+
 end
