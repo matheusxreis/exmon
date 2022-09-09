9e5eafb714a07a54e830baddbaeb987e1839035c
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 18:18:51 -0300

    feat: created Actions.Attack module and function attack_opponent, calculate_power and calculate_total_life

---
+++ lib/game/actions/attack.ex
@@ -0,0 +1,24 @@
+defmodule ExMon.Game.Actions.Attack do
+
+  alias ExMon.Game
+
+  @move_average_power 18..25
+  @move_random_power 10..35
+  def attack_opponent(target, move) do
+    damage =
+      move
+      |> calculate_power()
+
+    target
+      |> Game.fetch_player()
+      |> Map.get(:life)
+      |> calculate_total_life(damage)
+  end
+
+  defp calculate_power(:move_random), do: Enum.random(@move_random_power)
+  defp calculate_power(:move_average), do: Enum.random(@move_average_power)
+
+  defp calculate_total_life(life, damage) when life - damage < 0, do: 0
+  defp calculate_total_life(life, damage), do: life - damage
+
+end
