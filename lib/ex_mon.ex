a435d59d99739b52fc5ea6862d57c9b49ee3258e
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 22:18:28 -0300

    refactor: refactor start_game function, using pipe operator and constant module variable

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,12 +2,15 @@ defmodule ExMon do
   alias ExMon.Player
   alias ExMon.Game
 
+  @computer_name "TheRobotMonster"
+
   def create_player(n, m1, m2, m3) do
     Player.build(n, m1, m2, m3)
   end
 
   def start_game(player) do
-    computer_player = create_player("Vin Diesel", :punch, :kick, :heal)
-    Game.start(computer_player, player)
+    @computer_name
+    |> create_player(:punch, :kick, :angel_heal)
+    |> Game.start(player)
   end
 end
