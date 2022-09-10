949717890ae0617f7e993a5a777fc13f2dd04b96
Matheus Reis <matheus.reis081@gmail.com>
2022-09-09 23:50:58 -0300

    test: ensure that make_move/1 from ExMon module works correctly

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -42,9 +42,44 @@ defmodule ExMonTest do
   end
 
   describe "make_move/1" do
-    test "a2" do
-      assert 1+1 == 2
+    setup do
+      p = ExMon.create_player(
+        "Matheus",
+        :punch,
+        :kick,
+        :heal)
+
+        capture_io(fn ->
+          ExMon.start_game(p);
+        end)
+
+        :ok
     end
+    test "when the move is valid, do the move and the computer make a move" do
+
+      message =
+        capture_io(fn ->
+          assert ExMon.make_move(:kick) == :ok
+        end)
+
+      assert message =~ "The player attacked the computer"
+      assert message =~ "It's computer turn."
+      assert message =~ "It's player turn."
+      assert message =~ "status: :continue"
+
+    end
+    test "when the move is invalid, do the move and the computer make a move" do
+
+      message =
+        capture_io(fn ->
+          assert ExMon.make_move(:chute) == :ok
+        end)
+
+      assert message =~ "Invalid move: chute. This move doesn't exist."
+
+
+    end
+    
   end
 
 end
